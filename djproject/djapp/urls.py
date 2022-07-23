4e5b7226ff1253fac95047aaed63127a40bdebb2
w-a-gomes <wildenalisson@outlook.com>
2022-07-23 19:56:57 -0300

    origin

--- djproject/djapp/urls.py
+++ djproject/djapp/urls.py
@@ -6,7 +6,9 @@ admin_page_sufix = AppConfig.objects.get(identifier='general').admin_page_sufix
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('user', views.home, name='home'),
+    
+    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
 
     path('admin{}'.format(admin_page_sufix), views.admin, name='admin'),
     path('admin{}/'.format(admin_page_sufix), views.admin, name='admin'),
--- djproject/djapp/views.py
+++ djproject/djapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import AppConfig
 
+
 def index(request):
+    """General home page"""
     return render(
         request, 'djapp/index.html',
         {
@@ -12,6 +14,7 @@ def index(request):
 
 
 def home(request):
+    """User home page"""
     return render(
         request, 'djapp/home.html',
         {
@@ -22,6 +25,7 @@ def home(request):
 
 
 def admin(request):
+    """Administrative authentication page"""
     return render(
         request, 'djapp/admin.html',
         {
@@ -29,3 +33,32 @@ def admin(request):
             'title_description': 'Admin auth page',
         }
     )
+
+
+# Errors
+# https://docs.djangoproject.com/pt-br/4.0/intro/tutorial03/#a-shortcut-get-object-or-404
+
+# Templates
+# https://docs.djangoproject.com/pt-br/4.0/intro/tutorial03/#use-the-template-system
+
+# url
+# https://docs.djangoproject.com/pt-br/4.0/intro/tutorial03/#removing-hardcoded-urls-in-templates
+
+# Namespace url
+# https://docs.djangoproject.com/pt-br/4.0/intro/tutorial03/#namespacing-url-names
+
+# Atalhos
+# https://www.w3schools.com/html/html_computercode_elements.asp
+
+# Designe Responsivo
+# https://www.w3schools.com/html/html_responsive.asp
+
+# Tags
+# https://dev.to/thepylot/how-to-add-tags-to-your-models-in-django-django-packages-series-1-3704
+
+# Login
+# https://learndjango.com/tutorials/django-login-and-logout-tutorial
+# https://docs.djangoproject.com/pt-br/4.0/topics/auth/
+
+# HTML editor
+#
